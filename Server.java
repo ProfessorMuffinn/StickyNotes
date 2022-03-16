daa0e217113293404981e144739a6f9a3b54792a
ProfessorMuffinn <qbrimey@gmail.com>
2022-03-16 18:35:45 -0400

    client and server

--- Server.java
+++ Server.java
@@ -1,3 +1,17 @@
+import java.io.*;  
+import java.net.*;  
 public class Server {
-    
+    public static void main(String[] args){ 
+        try{
+            ServerSocket ss = new ServerSocket(6666); //Define socket port
+            Socket s = ss.accept();
+            DataInputStream message = new DataInputStream(s.getInputStream());
+            String  str = (String)message.readUTF();  
+            System.out.println("message= "+str);  
+            ss.close();  
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
